1044248d504f0758bff465364331cb53d52333fa
qthibodeaux <60246734+qthibodeaux@users.noreply.github.com>
2022-10-03 23:58:10 -0500

    Beginning updates and clean up

---
+++ src/components/Hero.js
@@ -0,0 +1,12 @@
+import HeroPic from '../assets/hero.jpg'
+import './Hero.css'
+
+function Hero() {
+  return (
+    <div className='herodiv'>
+        Hero
+    </div>
+  )
+}
+
+export default Hero
