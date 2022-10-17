c3501335e5dc021033cf26aaa28f852021acdeb9
qthibodeaux <60246734+qthibodeaux@users.noreply.github.com>
2022-10-16 23:58:17 -0500

    Updated to contacts page

--- src/components/items/Contact.js
+++ src/components/items/Contact.js
@@ -4,14 +4,25 @@ import './Contact.css'
 function Contact() {
   return (
     <div className='contactContainer'>
-      <div className='flowerDiv'>
-        <img src={Flower} alt="flowerimage" className='flowerImg'/>
+      <div className='contactLeft'>
+        <div className='flowerDiv'>
+          <img src={Flower} alt="flowerimage" className='flowerImg'/>
+        </div>
       </div>
-      <div>
-        <form>
-          <label for='name'>Name: *</label>
-          <input type='text' id='name' placeholder='Name' />
-          <input type='submit' />
+      <div className='contactRight'>
+        <form className='formDiv'>
+          <label for='name'>Name:</label>
+          <input type='text' id='name' placeholder='Name' required/>
+          
+          <label for='name'>Email:</label>
+          <input type='text' id='name' placeholder='Email' required/>
+          
+          <label for='name'>Message:</label>
+          <textarea name='message' rows='7' required></textarea>
+          
+          <div>
+            <button type='submit'>Send Message</button>
+          </div>
         </form>
       </div>
     </div>
