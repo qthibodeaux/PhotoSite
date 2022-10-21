85b5668f6154f52884a25952940cf23194a26dc7
qthibodeaux <60246734+qthibodeaux@users.noreply.github.com>
2022-10-20 23:58:55 -0500

    Adjusting the services cards

--- src/components/items/Services.js
+++ src/components/items/Services.js
@@ -1,8 +1,69 @@
-import React from 'react'
+import "./Services.css"
 
 function Services() {
   return (
-    <div>Services</div>
+    <div className='servicesContainer'>
+      <div className='heroContainer'></div>
+      <div className='itemsContainer'>
+        <div className='itemsRow'>
+          <div className="itemsCard">
+            <h1>Style-In-Motion Session</h1>
+            <ul>
+              <li>For dancers and athletes</li>
+              <li>Designer fashion, styling motion</li>
+              <li>Three Hour Session</li>
+            </ul>
+            <button>Learn More</button>
+          </div>
+
+          <div className="itemsCard">
+            <h1>Fashion/Magazine Project</h1>
+            <ul>
+              <li>Artistic development, photography, post-production</li>
+              <li>On-location or in-house studio</li>
+            </ul>
+            <button>Learn More</button>
+          </div>
+
+          <div className="itemsCard">
+            <h1>Graduation and event sessions</h1>
+            <ul>
+              <li>Editorial, fashion, styling</li>
+              <li>On-location or in-house studio</li>
+            </ul>
+            <button>Learn More</button>
+          </div>
+
+          <div className="itemsCard">
+            <h1>Party and banquet session</h1>
+            <ul>
+              <li>For dancers and athletes</li>
+              <li>On-location or in-house studio</li>
+              <li>Three Hour Session</li>
+            </ul>
+            <button>Learn More</button>
+          </div>
+
+          <div className="itemsCard">
+            <h1>Standard Session</h1>
+            <ul>
+              <li>Portraits and touch ups</li>
+              <li>On-location or in-house studio</li>
+            </ul>
+            <button>Learn More</button>
+          </div>
+
+          <div className="itemsCard">
+            <h1>Commercial Project</h1>
+            <ul>
+              <li>Editiorial, fashion, product</li>
+              <li>On-location or in-house studio</li>
+            </ul>
+            <button>Learn More</button>
+          </div>
+        </div>  
+      </div>
+    </div>
   )
 }
 
